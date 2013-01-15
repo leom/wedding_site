02a617fa876929b816afeba95b158e81710e766d
leo mendoza <leomendoza@gmail.com>
2013-01-14 22:21:32 -0500

    passenger_wsgi and __init__.py added for easier fabric deployment to DreamHost

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,14 @@
 from fabric.api import local, settings, run, cd, env
 from fabric.operations import put
+from fabric.contrib.project import rsync_project
 from fabric.contrib.console import confirm
 
 def deploy():
     remote_dir = '%s/flask_env' % env.remote_home
     run('touch %s/tmp/restart.txt' % remote_dir)
     run('source flask_env/bin/activate && pip install -r %s/wedding_site/requirements.txt' % remote_dir)
+    run('find %s/flask_env -iname "*.pyc" -exec rm {} \;' % env.remote_home)
 
-    put('*.py', '%s/wedding_site' % remote_dir)
-    put('*.ini', '%s/wedding_site' % remote_dir)
-    put('*.txt', '%s/wedding_site' % remote_dir)
-    with cd('%s/wedding_site' % remote_dir):
-        run('mkdir -p templates')
-        put('templates/*.html', 'templates')
-
-        run('mkdir -p static/bootstrap static/img')
-        put('static/bootstrap/*', 'static/bootstrap')
-        put('static/img/*', 'static/img')
-        put('static/*.css', 'static')
-        put('static/*.js', 'static')
-
-        run('mkdir -p migrations')
-        put('./migrations/*.py', 'migrations')
-        put('./migrations/versions/*.py', 'migrations/versions')
-        put('./migrations/*.mako', 'migrations')
+    rsync_project(remote_dir='%s/wedding_site' % remote_dir, local_dir='./', delete=True, exclude=['*.pyc', '*.ini', '.*', '*.psd', 'passenger_wsgi.py'])
+    put('alembic.ini', remote_dir)
+    put('passenger_wsgi.py', remote_dir)
---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+import sys, os
+INTERP = os.path.join(os.environ['HOME'], 'flask_env', 'bin', 'python')
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+sys.path.insert(0, os.getcwd())
+sys.path.insert(0, os.path.join(os.environ['HOME'], 'flask_env', 'wedding_site'))
+from wedding_site.main import app as application
+
+# Uncomment next two lines to enable debugging
+from werkzeug.debug import DebuggedApplication
+application = DebuggedApplication(application, evalex=True)
