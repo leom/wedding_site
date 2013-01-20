b51621e66f9bc1fa1c20f9b1672d5d811c1cf843
leo mendoza <leomendoza@gmail.com>
2013-01-20 00:55:38 -0500

    moved wsgi touch to after the file move. duh

--- fabfile.py
+++ fabfile.py
@@ -5,10 +5,11 @@ from fabric.contrib.console import confirm
 
 def deploy():
     remote_dir = '%s/flask_env' % env.remote_home
-    run('touch %s/tmp/restart.txt' % remote_dir)
     run('source flask_env/bin/activate && pip install -r %s/wedding_site/requirements.txt' % remote_dir)
     run('find %s/flask_env -iname "*.pyc" -exec rm {} \;' % env.remote_home)
 
     rsync_project(remote_dir='%s/wedding_site' % remote_dir, local_dir='./', delete=True, exclude=['*.pyc', '*.ini', '.*', '*.psd', 'passenger_wsgi.py'])
     put('alembic.ini', remote_dir)
     put('passenger_wsgi.py', remote_dir)
+
+    run('touch %s/tmp/restart.txt' % remote_dir)
