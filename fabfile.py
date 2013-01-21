40b6d0f0857315be1603282dac6949ef5154f19e
leo mendoza <leomendoza@gmail.com>
2013-01-21 14:33:18 -0500

    fabfile now installs requirements *after* rsyncing the new file. duh

--- fabfile.py
+++ fabfile.py
@@ -5,11 +5,9 @@ from fabric.contrib.console import confirm
 
 def deploy():
     remote_dir = '%s/flask_env' % env.remote_home
-    run('source flask_env/bin/activate && pip install -r %s/wedding_site/requirements.txt' % remote_dir)
     run('find %s/flask_env -iname "*.pyc" -exec rm {} \;' % env.remote_home)
-
     rsync_project(remote_dir='%s/wedding_site' % remote_dir, local_dir='./', delete=True, exclude=['*.pyc', '*.ini', '.*', '*.psd', 'passenger_wsgi.py', 'playme/*', '*.mp3', '*.m4a'])
-    run('touch %s/tmp/restart.txt' % remote_dir)
+    run('source flask_env/bin/activate && pip install -r %s/wedding_site/requirements.txt && touch %s/tmp/restart.txt' % (remote_dir, remote_dir))
 
 def setup_config():
     remote_dir = '%s/flask_env' % env.remote_home
