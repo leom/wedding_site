41a2ac9e523b24e132c9dec683f4a9b58f2bc3cf
leo mendoza <leomendoza@gmail.com>
2013-01-20 23:26:54 -0500

    from postgres to mysql

--- manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(url='postgresql://localhost/wedding_site', debug='False', repository='wedding_site_migrations')
